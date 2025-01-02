af2cb9c171079f9c3ff3b33f63b001a8078f1b8f
Matei Sirghe <matei.sirghe@gmail.com>
2025-01-02 02:11:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -967,3 +967,9 @@ function animate() {
     }
 }
 renderer.setAnimationLoop(animate);
+
+function onWindowResize(){
+    camera.aspect=window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth,window.innerHeight);
+}
